351d36617a40d5d9df7f66120d53ded4e11abe28
mehmetnuricavdar <mn.cavdar21@gmail.com>
2022-10-17 14:25:40 +0200

    exercise

--- Coursera_Exercises/Code_Together02/JS_Flow-Control/script.js
+++ Coursera_Exercises/Code_Together02/JS_Flow-Control/script.js
@@ -1,10 +1,10 @@
-console.log('Hello World!')
+// console.log("Hello World!");
 
-let name = prompt("What is your name?")
+// let name = prompt("What is your name?");
 
-if (name.length != 0){
-    console.log(`Hello ${name}`)
-}
+// if (name.length != 0) {
+//   document.write(`Hello ${name}`);
+// } else document.write("Feeling Shy?");
 
 let grades = [98, 76, 88, 74, 56, 90, 45, 60, 78, 40, 100];
 
@@ -16,4 +16,21 @@ if (grades.length > 0) {
   }
 
   console.log(sum / grades.length);
+  document.write(sum / grades.length);
+} else document.write("Empty Array");
+
+document.write("skipping Empty Array element");
+
+let grades2 = [2, 4, , , 4, , 8, , 5, 6, 7];
+let sum2 = 0;
+let count = 0;
+
+if (grades2.length > 0) {
+  for (index = 0; index < grades2.length; index++)
+    if (grades2[index] != undefined) {
+      sum2 += grades2[index];
+      count += 1;
+    }
 }
+
+document.write(sum2 / count);
