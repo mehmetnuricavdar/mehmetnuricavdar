2e290cb43d8989bf9cb852ad256420bfbb25c37d
Mehmet Nuri Cavdar <113375790+mehmetnuricavdar@users.noreply.github.com>
2023-03-10 11:28:31 +0100

    paretn child in React

---
+++ Coursera_Exercises/META_React/React_Basics/parent-child/src/App.js
@@ -0,0 +1,13 @@
+import Child from "./Child";
+import "./App.css";
+
+function App() {
+  const date = new Date();
+  return (
+    <div>
+      <Child message={date.toLocaleTimeString()} />
+    </div>
+  );
+}
+
+export default App;
